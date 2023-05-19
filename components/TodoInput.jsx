7ec7464134be0c28d70d1ac2071c8387421166ac
minhthangchan3110 <92298373+minhthangchan3110@users.noreply.github.com>
2023-05-19 15:50:47 +0700

    Update TodoInput.jsx

--- components/TodoInput.jsx
+++ components/TodoInput.jsx
@@ -4,26 +4,25 @@ import { addTodo } from './reducers/todoSlice';
 
 const TodoInput = () => {
   const dispatch = useDispatch();
-  const [inputValue, setInputValue] = useState('');
+  const [text, setText] = useState('');
 
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-  };
-
-  const handleAddTodo = () => {
-    dispatch(addTodo({
-      id: new Date().getTime(),
-      content: inputValue,
-      isCompleted: false,
-    }));
-    setInputValue('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim() !== '') {
+      dispatch(addTodo({
+        id: Date.now(),
+        text,
+        isCompleted: false,
+      }));
+      setText('');
+    }
   };
 
   return (
-    <div>
-      <input type="text" value={inputValue} onChange={handleInputChange} />
-      <button onClick={handleAddTodo}>Add Todo</button>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter a todo" />
+      <button type="submit">Add Todo</button>
+    </form>
   );
 };
 
